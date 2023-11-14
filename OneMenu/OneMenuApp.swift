0f7ec8a7861a8f88d1adb728cd56169a34050566
2whitewolf <bogdansh.02@gmail.com>
2023-11-14 11:42:25 +0200

    start project

---
+++ OneMenu/OneMenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  OneMenuApp.swift
+//  OneMenu
+//
+//  Created by iMacRoman on 14.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct OneMenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
